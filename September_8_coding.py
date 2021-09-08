5f0b497dc7b15e4ec1ec1fd1ed5d9d5827c5f757
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2021-09-08 17:37:02 -0400

    Add files via upload

---
+++ September_8_coding.py
@@ -0,0 +1,51 @@
+#Python coding from class September 8
+
+# This is a comment. Python ignores it; it is not executed
+
+# from last class
+print ("hello, world")  # this is called a literal - exactly, this
+
+#using a variable
+a = "hello, world"
+print(a)
+
+b = 5
+pi = 3.14
+correct = True
+print(b)
+print(pi)
+print (correct)
+
+PI = 3.14159
+PI = PI + 1
+print(PI)
+
+Pi = 3
+pI = 9.2
+print("pi is ", pi, "PI is ", PI, "Pi is ", Pi, "and pI is ", pI)
+
+
+######################################
+#
+# Name: Prof. Arsenault
+# Section: 60
+#
+#
+#######################################
+
+"""
+
+everything in between here is considered a comment.
+You don't need the #
+
+
+"""
+
+"""
+Name: Prof. Arsenault
+Section: 60
+Date: ...
+
+"""
+
+
